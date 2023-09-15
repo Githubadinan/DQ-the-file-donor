48f53c16251fe722c4e73ea7084294585c68aeda
Githubadinan <145027702+Githubadinan@users.noreply.github.com>
2023-09-15 23:00:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jo:Jo@cluster0.otu4xt5.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://XP:XP@cluster0.eofjslt.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Mangodb")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
